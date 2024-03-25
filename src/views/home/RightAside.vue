c599b07157d20ca012edd88a948606c78c2e9292
silengzi <474960201@qq.com>
2024-03-25 18:04:13 +0800

    画页面

--- src/views/home/RightAside.vue
+++ src/views/home/RightAside.vue
@@ -3,43 +3,196 @@
     <dv-border-box-10 backgroundColor="rgb(4, 47, 84, 0.7)">
       <div class="wrapper">
         <div class="top">
-          <dv-decoration-7 class="titleDecoration" style="width:150px;height:30px;">
+          <dv-decoration-7
+            class="titleDecoration"
+            style="width:150px;height:30px;"
+          >
             <div class="title">
-              优良天数
+              告警统计
             </div>
           </dv-decoration-7>
           <dv-decoration-10 style="width:100%;height:5px;" />
-          </div>
+          <!-- 饼图 -->
+          <div
+            class="pie-chart"
+            ref="pieChart"
+            style="width: 100%; height: 80%"
+          ></div>
+        </div>
         <div class="center">
-          <dv-decoration-7 class="titleDecoration" style="width:150px;height:30px;">
+          <dv-decoration-7
+            class="titleDecoration"
+            style="width:150px;height:30px;"
+          >
             <div class="title">
-              质量排名
+              古树名木
             </div>
           </dv-decoration-7>
           <dv-decoration-10 style="width:100%;height:5px;" />
-          </div>
+          <div
+            class="bar-chart"
+            ref="barChart"
+            style="width: 100%; height: 80%"
+          ></div>
+        </div>
         <div class="bottom">
-          <dv-decoration-7 class="titleDecoration" style="width:150px;height:30px;">
+          <dv-decoration-7
+            class="titleDecoration"
+            style="width:150px;height:30px;"
+          >
             <div class="title">
               污染日历
             </div>
           </dv-decoration-7>
           <dv-decoration-10 style="width:100%;height:5px;" />
-          </div>
+        </div>
       </div>
     </dv-border-box-10>
   </div>
 </template>
 
 <script>
+// 引入echarts
+import * as echarts from 'echarts'
 export default {
   name: '',
   data() {
     return {
-
+      // 饼图数据
+      pieData: [
+        { value: 335, name: '火灾告警' },
+        { value: 310, name: '非法活动' },
+        { value: 234, name: '野生动物异常' },
+        { value: 350, name: '自然灾害' },
+        { value: 555, name: '环境污染' }
+      ],
+      // 柱状图数据
+      barXData: ['乔木', '灌木', '草本'],
     };
   },
+  mounted() {
+    this.renderPieChart()
+    this.renderBarChart()
+  },
   methods: {
+    // 饼图渲染
+    renderPieChart() {
+      const pieChart = echarts.init(this.$refs.pieChart)
+      const option = {
+        tooltip: {
+          trigger: 'item'
+        },
+        legend: {
+          type: 'scroll',
+          data: ['火灾告警', '非法活动', '野生动物异常', '自然灾害', '环境污染'],
+          top: 'bottom',
+          left: '20',
+          textStyle: {
+            color: '#fff'
+          }
+        },
+        toolbox: {
+          show: false,
+          feature: {
+            // mark: { show: true },
+            // dataView: { show: true, readOnly: false },
+            // restore: { show: true },
+            // saveAsImage: { show: true }
+          }
+        },
+        series: [
+          {
+            name: '告警统计',
+            type: 'pie',
+            radius: [30, 70],
+            center: ['50%', '50%'],
+            // roseType: 'area',
+            // itemStyle: {
+            //   borderRadius: 8
+            // },
+            data: this.pieData,
+            emphasis: {
+              itemStyle: {
+                shadowBlur: 10,
+                shadowOffsetX: 0,
+                shadowColor: 'rgba(0, 0, 0, 0.5)'
+              }
+            }
+          }
+        ]
+      };
+      pieChart.setOption(option);
+    },
+    // 柱状图渲染
+    renderBarChart() {
+      const barChart = echarts.init(this.$refs.barChart)
+      const option = {
+        xAxis: {
+          type: 'category',
+          data: this.barXData,
+          // 坐标轴样式
+          axisLine: {
+            lineStyle: {
+              color: '#9daab8'
+            }
+          },
+          // 坐标轴标签样式
+          axisLabel: {
+            color: '#fff'
+          }
+        },
+        yAxis: {
+          type: 'value',
+          
+          // 坐标轴样式
+          axisLine: {
+            show: true,
+            lineStyle: {
+              color: '#9daab8'
+            }
+          },
+          // 坐标轴标签样式
+          axisLabel: {
+            color: '#fff'
+          },
+          // 显示分割线
+          splitLine: {
+            show: false,
+          }
+        },
+        series: [
+          {
+            data: [
+              {
+                value: 100,
+                itemStyle: {
+                  color: 'rgb(0, 255, 255)',
+                }
+              },
+              {
+                value: 156,
+                itemStyle: {
+                  color: 'rgb(255, 192, 203)',
+                }
+              },
+              {
+                value: 145,
+                itemStyle: {
+                  color: 'rgb(255, 255, 192)',
+                }
+              },
+            ],
+            // 柱宽
+            barWidth: '40%',
+            type: 'bar',
+            itemStyle: {
+              color: 'rgb(0, 255, 255)'
+            }
+          }
+        ]
+      }
+      barChart.setOption(option);
+    }
 
   }
 };
